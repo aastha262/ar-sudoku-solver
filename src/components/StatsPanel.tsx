0541f34ed13a29595ba81c6039257024314c695c
Virat Dharaiya <viratdharaiya2004@gmail.com>
2024-05-31 23:38:06 +0530

    Change in HTML

--- src/components/StatsPanel.tsx
+++ src/components/StatsPanel.tsx
@@ -22,28 +22,28 @@ export default function StatsPanel({
   return (
     <div className="stats-panel">
       <div>
-        <b>imageCaptureTime:</b> {Math.round(imageCaptureTime)}
+        <b>image Capture Time:</b> {Math.round(imageCaptureTime)}
       </div>
       <div>
-        <b>thresholdTime:</b> {Math.round(thresholdTime)}
+        <b>threshold Time:</b> {Math.round(thresholdTime)}
       </div>
       <div>
-        <b>connectedComponentTime:</b> {Math.round(connectedComponentTime)}
+        <b>connected Component Time:</b> {Math.round(connectedComponentTime)}
       </div>
       <div>
-        <b>getCornerPointsTime:</b> {Math.round(getCornerPointsTime)}
+        <b>get CornerPoints Time:</b> {Math.round(getCornerPointsTime)}
       </div>
       <div>
-        <b>extractImageTime:</b> {Math.round(extractImageTime)}
+        <b>extract Image Time:</b> {Math.round(extractImageTime)}
       </div>
       <div>
-        <b>extractBoxesTime:</b> {Math.round(extractBoxesTime)}
+        <b>extract Boxes Time:</b> {Math.round(extractBoxesTime)}
       </div>
       <div>
-        <b>ocrTime:</b> {Math.round(ocrTime)}
+        <b>ocr Time:</b> {Math.round(ocrTime)}
       </div>
       <div>
-        <b>solveTime:</b> {Math.round(solveTime)}
+        <b>solve Time:</b> {Math.round(solveTime)}
       </div>
     </div>
   );
